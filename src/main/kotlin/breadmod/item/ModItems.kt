97cb50754304ccb8339a1180d62862ab4db02a45
ttttdoy <logan3mclean@gmail.com>
2023-11-30 21:32:54 -0500

    uhh 2

--- src/main/kotlin/breadmod/item/ModItems.kt
+++ src/main/kotlin/breadmod/item/ModItems.kt
@@ -18,8 +18,15 @@ object ModItems {
         BlockItem(ModBlocks.BREAD_BLOCK, Item.Properties().stacksTo(64).tab(ModCreativeTab)) }
     val TEST_BREAD by REGISTRY.registerObject("test_bread") {
         TestBreadItem() }
+    // I somehow reverted every change i made since last commit im gonna AAAAA
     val BREAD_HELMET by REGISTRY.registerObject("bread_helmet") {
         ArmorItem(ArmorTiers.BREAD_ARMOR, EquipmentSlot.HEAD, Item.Properties().tab(ModCreativeTab)) }
+    val BREAD_CHESTPLATE by REGISTRY.registerObject("bread_chestplate") {
+    ArmorItem(ArmorTiers.BREAD_ARMOR, EquipmentSlot.CHEST, Item.Properties().tab(ModCreativeTab)) }
+    val BREAD_LEGGINGS by REGISTRY.registerObject("bread_legggings") {
+        ArmorItem(ArmorTiers.BREAD_ARMOR, EquipmentSlot.LEGS, Item.Properties().tab(ModCreativeTab)) }
+    val BREAD_BOOTS by REGISTRY.registerObject("bread_boots") {
+        ArmorItem(ArmorTiers.BREAD_ARMOR, EquipmentSlot.FEET, Item.Properties().tab(ModCreativeTab)) }
 }
 
 
