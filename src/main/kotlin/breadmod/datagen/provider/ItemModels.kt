4cf82a304a85f5ff4e9fab28018f7e1b3f513b8f
ttttdoy <logan3mclean@gmail.com>
2023-12-01 22:44:36 -0500

    Update files part 2

--- src/main/kotlin/breadmod/datagen/provider/ItemModels.kt
+++ src/main/kotlin/breadmod/datagen/provider/ItemModels.kt
@@ -10,10 +10,7 @@ class ItemModels (
     fileHelper: ExistingFileHelper,
 ) : ItemModelProvider(generator, BreadMod.ID, fileHelper) {
     override fun registerModels() {
-//        singleTexture(Battery.id.path, mcLoc("item/generated"), "layer0", modLoc("item/battery"));
-
-
-        withExistingParent("breadmod:bread_block", modLoc("block/bread_block"))
+        //withExistingParent("breadmod:bread_block", modLoc("block/bread_block"))
         singleTexture("breadmod:test_bread", mcLoc("item/generated"), "layer0", modLoc("items/test_bread"))
     }
 }
--- src/main/kotlin/breadmod/datagen/provider/lang/USEnglishLanguageProvider.kt
+++ src/main/kotlin/breadmod/datagen/provider/lang/USEnglishLanguageProvider.kt
@@ -8,7 +8,10 @@ import net.minecraftforge.common.data.LanguageProvider
 
 class USEnglishLanguageProvider(generator: DataGenerator) : LanguageProvider(generator, BreadMod.ID, "en_us") {
     override fun addTranslations() {
+        add("itemGroup.${BreadMod.ID}", "The Bread Mod")
+        // Blocks
         add(ModItems.BREAD_BLOCK_ITEM, "Bread Block")
+        // Items
         add(ModItems.TEST_BREAD, "Test Bread")
     }
 }
