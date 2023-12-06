5c96f7e438161b84d7ddd9516b96e088e57d800e
ttttdoy <logan3mclean@gmail.com>
2023-12-05 20:41:05 -0500

    Potion effect testing

--- src/main/kotlin/breadmod/item/TestBreadItem.kt
+++ src/main/kotlin/breadmod/item/TestBreadItem.kt
@@ -7,14 +7,15 @@ import net.minecraft.ChatFormatting
 import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.effect.MobEffectInstance
+import net.minecraft.world.effect.MobEffects
+import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.food.FoodProperties
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
-import net.minecraft.world.level.Explosion
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.EntityHitResult
 
@@ -31,6 +32,14 @@ class TestBreadItem : Item(Properties().food(FoodProperties.Builder().nutrition(
             }
         }
         return super.use(pLevel, pPlayer, pHand)
+    }
+
+
+
+    override fun finishUsingItem(pStack: ItemStack, pLevel: Level, pLivingEntity: LivingEntity): ItemStack {
+        pLivingEntity.addEffect(MobEffectInstance(MobEffects.INVISIBILITY,200,0))
+
+        return super.finishUsingItem(pStack, pLevel, pLivingEntity)
     }
 
     override fun appendHoverText(
