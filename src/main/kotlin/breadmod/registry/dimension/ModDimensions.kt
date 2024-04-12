d20d3b202d5c97644999d8828f4f108e9a4a326e
meownium <31296383+ATPStorages@users.noreply.github.com>
2024-04-12 13:41:04 -0500

    Update ModDimensions.kt

--- src/main/kotlin/breadmod/registry/dimension/ModDimensions.kt
+++ src/main/kotlin/breadmod/registry/dimension/ModDimensions.kt
@@ -7,4 +7,20 @@ import net.minecraftforge.registries.DeferredRegister
 
 object ModDimensions {
     val deferredRegister: DeferredRegister<DimensionType> = DeferredRegister.create(Registries.DIMENSION_TYPE, BreadMod.ID)
+    val BREAD = deferredRegister.register("bread") { 
+        DimensionType(
+            OptionalLong.empty(),
+            true,
+            false,
+            true,
+            true,
+            4.0,
+            true,
+            false,
+            -50, // minY TODO,
+            100, // maxY TODO
+            100,
+            TODO("Figure this out later https://mcstreetguy.github.io/ForgeJavaDocs/1.20.1-latest/net/minecraft/world/level/dimension/DimensionType.html")
+        )
+    }
 }
