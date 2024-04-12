1b3b6917968b27a7326a486934e0ed88344c3ae6
ATPStorages <tcgytsc@gmail.com>
2024-04-11 19:43:48 -0500

    Make ArmorColor.kt work

--- src/main/kotlin/breadmod/item/colors/ArmorColor.kt
+++ src/main/kotlin/breadmod/item/colors/ArmorColor.kt
@@ -1,10 +1,10 @@
 package breadmod.item.colors
 
+import breadmod.util.getColor
 import net.minecraft.client.color.item.ItemColor
 import net.minecraft.world.item.ItemStack
+import java.awt.Color
 
 object ArmorColor: ItemColor {
-    override fun getColor(pStack: ItemStack, pTintIndex: Int): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getColor(pStack: ItemStack, pTintIndex: Int): Int = if(pTintIndex == 0) pStack.getColor().rgb else Color.WHITE.rgb
 }
