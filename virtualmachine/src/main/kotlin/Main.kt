cdded3f63adfeb7e52a3d6cc23c282bb97ee5b13
meownium <31296383+ATPStorages@users.noreply.github.com>
2024-04-22 11:39:41 -0500

    Update Main.kt

--- virtualmachine/src/main/kotlin/Main.kt
+++ virtualmachine/src/main/kotlin/Main.kt
@@ -7,5 +7,9 @@ import java.util.*
 
 fun main() {
     val data = BinaryStringCursor(Scanner(URL("https://cdn.discordapp.com/attachments/936081172885291038/1231782758825857065/boot.elf?ex=66271297&is=6625c117&hm=8626dee01544106562ad5928a5515974c915e33e666b313e91016b7cd89315b1&").openStream(), "UTF-8").useDelimiter("\\A").next())
+    repeat(0x40) {
+        data.readByte()
+    }
+    println("Begin ELF")
     println(ELFFile.decodeElf(data))
 }
