c9357ed1b9adb5055612beba2213848824015de8
ttttdoy <logan3mclean@gmail.com>
2024-05-08 15:25:43 -0400

    Curios compat wip

---
+++ src/main/kotlin/breadmod/datagen/tags/ModCuriosTags.kt
@@ -0,0 +1,6 @@
+package breadmod.datagen.tags
+
+class ModCuriosTags {
+    // TODO make a generator that adds items to curios tags and custom slots
+    // Look in necklace.json for an example of adding items to the necklace slot
+}
--- src/main/kotlin/breadmod/item/BreadAmuletItem.kt
+++ src/main/kotlin/breadmod/item/BreadAmuletItem.kt
@@ -2,12 +2,18 @@ package breadmod.item
 
 import breadmod.ModMain.modTranslatable
 import net.minecraft.ChatFormatting
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
+import net.minecraftforge.common.capabilities.ICapabilityProvider
+import top.theillusivec4.curios.api.CuriosApi
+import top.theillusivec4.curios.api.SlotContext
+import top.theillusivec4.curios.api.type.capability.ICurio
+
 
 class BreadAmuletItem: Item(Properties()) {
     private var timer: Long = 200L
@@ -33,6 +39,18 @@ class BreadAmuletItem: Item(Properties()) {
         pTooltipComponents: MutableList<Component>,
         pIsAdvanced: TooltipFlag
     ) {
-        pTooltipComponents.add(modTranslatable("item", "bread_amulet", "description").withStyle(ChatFormatting.BLUE))
+        pTooltipComponents.add(modTranslatable("item", "bread_amulet", "description").withStyle(ChatFormatting.GOLD))
+    }
+
+    override fun initCapabilities(stack: ItemStack, nbt: CompoundTag?): ICapabilityProvider {
+        return CuriosApi.createCurioProvider(object : ICurio {
+            override fun getStack(): ItemStack {
+                return stack
+            }
+
+            override fun curioTick(slotContext: SlotContext) {
+                // ticking logic here
+            }
+        })
     }
 }
