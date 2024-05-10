f43200bcfb1df989d70fe62454feab8a01467f4a
ttttdoy <logan3mclean@gmail.com>
2024-05-10 17:07:02 -0400

    better bread slice jei recipe

--- src/main/kotlin/breadmod/compat/jei/vanillaExtensions/JEIBreadSliceCraftingExtension.kt
+++ src/main/kotlin/breadmod/compat/jei/vanillaExtensions/JEIBreadSliceCraftingExtension.kt
@@ -5,33 +5,27 @@ import mezz.jei.api.gui.builder.IRecipeLayoutBuilder
 import mezz.jei.api.gui.ingredient.ICraftingGridHelper
 import mezz.jei.api.recipe.IFocusGroup
 import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension
+import net.minecraft.tags.ItemTags
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
+import net.minecraftforge.registries.ForgeRegistries
 
 class JEIBreadSliceCraftingExtension: ICraftingCategoryExtension {
     override fun getWidth(): Int = 2
     override fun getHeight(): Int = 1
-
-    // TODO there's gotta be a way to return a list of an item tag right?
-    private val swordSet = listOf(
-        Items.GOLDEN_SWORD,
-        Items.DIAMOND_SWORD,
-        Items.NETHERITE_SWORD,
-        Items.WOODEN_SWORD,
-        Items.IRON_SWORD,
-        ModItems.BREAD_SWORD.get(),
-        ModItems.RF_BREAD_SWORD.get()
-    ).map { it.defaultInstance }
 
     override fun setRecipe(
         builder: IRecipeLayoutBuilder,
         craftingGridHelper: ICraftingGridHelper,
         focuses: IFocusGroup)
     {
+        val swordTags = ForgeRegistries.ITEMS.tags()?.getTag(ItemTags.SWORDS) ?: listOf()
+        val swordList = swordTags.map { it.defaultInstance }
+
         craftingGridHelper.createAndSetInputs(
             builder,
             listOf(
-                swordSet,
+                swordList,
                 listOf(Items.BREAD.defaultInstance)
             ), width, height
         )
