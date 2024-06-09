f682aa87246297c1410997d6932fdd5ff634cf8c
ATPStorages <900600masterplayer@gmail.com>
2024-06-09 01:06:33 -0500

    Data class loading, using Java reflection instead

--- src/main/kotlin/breadmod/datagen/toolgun/ModToolgunModeDataLoader.kt
+++ src/main/kotlin/breadmod/datagen/toolgun/ModToolgunModeDataLoader.kt
@@ -7,7 +7,6 @@ import net.minecraft.server.packs.resources.ResourceManager
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener
 import net.minecraft.util.profiling.ProfilerFiller
 import org.jetbrains.annotations.ApiStatus.Internal
-import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.primaryConstructor
 
 @Internal
@@ -25,7 +24,7 @@ internal object ModToolgunModeDataLoader : SimpleJsonResourceReloadListener(Gson
             if(location.path.startsWith("mode/")) {
                 val classSet = loadedModes.getOrPut(location.namespace) { mutableSetOf() }
                 val loadedClass = ClassLoader.getSystemClassLoader().loadClass(data.asJsonObject.getAsJsonPrimitive("class").asString).kotlin
-                if(loadedClass.isSubclassOf(IToolgunMode::class)) {
+                if(loadedClass.java.isAssignableFrom(IToolgunMode::class.java)) {
                     classSet.add(loadedClass.primaryConstructor!!.call() as IToolgunMode)
                 } else throw IllegalArgumentException("Class parameter for toolgun mode $location is invalid. Loaded an instance of ${loadedClass.qualifiedName}, expected a subclass of ${IToolgunMode::class.qualifiedName}")
             }
