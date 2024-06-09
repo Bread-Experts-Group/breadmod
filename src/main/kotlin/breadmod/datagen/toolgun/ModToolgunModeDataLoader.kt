172d3ff702f02c2d60da784a56f02806756b9821
ATPStorages <900600masterplayer@gmail.com>
2024-06-09 01:24:06 -0500

    Suppress type checking until I can solve this.

--- src/main/kotlin/breadmod/datagen/toolgun/ModToolgunModeDataLoader.kt
+++ src/main/kotlin/breadmod/datagen/toolgun/ModToolgunModeDataLoader.kt
@@ -7,6 +7,9 @@ import net.minecraft.server.packs.resources.ResourceManager
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener
 import net.minecraft.util.profiling.ProfilerFiller
 import org.jetbrains.annotations.ApiStatus.Internal
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
 
 @Internal
 internal object ModToolgunModeDataLoader : SimpleJsonResourceReloadListener(Gson(), "toolgun") {
@@ -22,13 +25,13 @@ internal object ModToolgunModeDataLoader : SimpleJsonResourceReloadListener(Gson
         pObject.forEach { (location, data) ->
             if(location.path.startsWith("mode/")) {
                 val classSet = loadedModes.getOrPut(location.namespace) { mutableSetOf() }
-                val loadedClass = ClassLoader.getSystemClassLoader().loadClass(data.asJsonObject.getAsJsonPrimitive("class").asString)
-                if(IToolgunMode::class.java.isAssignableFrom(loadedClass)) {
-                    val classConstructor = loadedClass.constructors.first()
+                val loadedClass = Class.forName(data.asJsonObject.getAsJsonPrimitive("class").asString).kotlin
+                //if(loadedClass.isSubclassOf(IToolgunMode::class)) {
+                    val classConstructor = loadedClass.primaryConstructor!!
                     classConstructor.isAccessible = true
-                    classSet.add(classConstructor.newInstance() as IToolgunMode)
+                    classSet.add(classConstructor.call() as IToolgunMode)
                     classConstructor.isAccessible = false
-                } else throw IllegalArgumentException("Class parameter for toolgun mode $location is invalid. Loaded an instance of ${loadedClass.kotlin.qualifiedName}, expected a subclass of ${IToolgunMode::class.qualifiedName}")
+                //} else throw IllegalArgumentException("Class parameter for toolgun mode $location is invalid. Loaded an instance of ${loadedClass.kotlin.qualifiedName}, expected a subclass of ${IToolgunMode::class.qualifiedName}")
             }
         }
         println("Here's the classes I loaded: $loadedModes")
