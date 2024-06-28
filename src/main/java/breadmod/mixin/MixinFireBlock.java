ca12b9072be0922cc11202af5d5b1432fb7b9bbf
ttttdoy <logan3mclean@gmail.com>
2024-06-28 00:45:58 -0400

    ok now it launches

--- src/main/java/breadmod/mixin/MixinFireBlock.java
+++ src/main/java/breadmod/mixin/MixinFireBlock.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FireBlock.class)
 abstract class MixinFireBlock {
-    @Inject(method = "tryCatchFire", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "tryCatchFire", at = @At("HEAD"), cancellable = true, remap = false)
     private void tryCatchFire(Level p_53432_, BlockPos p_53433_, int p_53434_, RandomSource p_53435_, int p_53436_, Direction face, CallbackInfo ci) {
         BlockState blockState = p_53432_.getBlockState(p_53433_);
         if(blockState.getBlock().getClass().isAnnotationPresent(ISpecialFireAction.class)) {
