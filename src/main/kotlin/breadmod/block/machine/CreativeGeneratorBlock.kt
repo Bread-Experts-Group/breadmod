a58838adf1a427301423f139b4dd5ed54e366fac
ttttdoy <logan3mclean@gmail.com>
2024-07-17 00:37:09 -0400

    creative generator model

--- src/main/kotlin/breadmod/block/machine/CreativeGeneratorBlock.kt
+++ src/main/kotlin/breadmod/block/machine/CreativeGeneratorBlock.kt
@@ -2,18 +2,19 @@ package breadmod.block.machine
 
 import breadmod.block.machine.entity.CreativeGeneratorBlockEntity
 import breadmod.registry.block.ModBlockEntities
+import net.minecraft.core.BlockPos
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
 
-// TODO: model for creative generator
 class CreativeGeneratorBlock: BaseAbstractMachineBlock.Powered<CreativeGeneratorBlockEntity>(
     ModBlockEntities.CREATIVE_GENERATOR,
-    Properties.of(),
+    Properties.of().noOcclusion(),
     false
 ) {
     override fun adjustBlockStateDefinition(pBuilder: StateDefinition.Builder<Block, BlockState>) {
@@ -22,6 +23,11 @@ class CreativeGeneratorBlock: BaseAbstractMachineBlock.Powered<CreativeGenerator
     override fun getStateForPlacement(pContext: BlockPlaceContext): BlockState? =
         defaultBlockState()
             .setValue(BlockStateProperties.HORIZONTAL_FACING, pContext.horizontalDirection.opposite)
+
+    // todo toggling logic with redstone
+    override fun onNeighborChange(state: BlockState, level: LevelReader, pos: BlockPos, neighbor: BlockPos) {
+        super.onNeighborChange(state, level, pos, neighbor)
+    }
 
     override fun getServerTicker(pLevel: Level, pState: BlockState): BlockEntityTicker<CreativeGeneratorBlockEntity> =
         BlockEntityTicker { tLevel, tPos, tState, tBlockEntity -> tBlockEntity.tick(tLevel, tPos, tState, tBlockEntity) }
--- src/main/kotlin/breadmod/block/machine/entity/CreativeGeneratorBlockEntity.kt
+++ src/main/kotlin/breadmod/block/machine/entity/CreativeGeneratorBlockEntity.kt
@@ -3,11 +3,15 @@ package breadmod.block.machine.entity
 import breadmod.registry.block.ModBlockEntities
 import breadmod.util.capability.EnergyBattery
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.sounds.SoundSource
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
 import net.minecraftforge.common.capabilities.ForgeCapabilities
 
+// todo block entity renderer for mini sun inside of model
 class CreativeGeneratorBlockEntity(
     pPos: BlockPos,
     pBlockState: BlockState,
@@ -15,7 +19,8 @@ class CreativeGeneratorBlockEntity(
     ModBlockEntities.CREATIVE_GENERATOR.get(),
     pPos,
     pBlockState,
-    ForgeCapabilities.ENERGY to (EnergyBattery(Int.MAX_VALUE, 0, Int.MAX_VALUE) to null)
+    ForgeCapabilities.ENERGY to (EnergyBattery(Int.MAX_VALUE, 0, Int.MAX_VALUE) to
+            mutableListOf(null, Direction.DOWN, Direction.EAST, Direction.WEST, Direction.NORTH))
 ) {
     override fun postTick(
         pLevel: Level,
@@ -26,10 +31,16 @@ class CreativeGeneratorBlockEntity(
         pBlockEntity.capabilityHolder.capabilityOrNull<EnergyBattery>(ForgeCapabilities.ENERGY).let {
             it?.stored = Int.MAX_VALUE
         }
+//        println(pLevel.gameTime % 80.0)
+
+        if(pLevel.gameTime % 80.0 == 0.0) {
+            // todo toggleable state that plays the beacon deactivate and activate beacon
+            pLevel.playSound(null, pPos, SoundEvents.BEACON_AMBIENT, SoundSource.BLOCKS, 1.0f, 1.0f)
+        }
 
         val sides = (capabilityHolder.capabilities[ForgeCapabilities.ENERGY] ?: return).second
         capabilityHolder.capability<EnergyBattery>(ForgeCapabilities.ENERGY).distribute(
-            pLevel, pPos, sides, pState.getValue(BlockStateProperties.HORIZONTAL_FACING)
+            pLevel, pPos, sides, pState.getValue(BlockStateProperties.HORIZONTAL_FACING).opposite
         )
     }
 }
