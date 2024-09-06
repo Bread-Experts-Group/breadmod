5678b63393be02f6849c6b5c275ff7ab3832eb31
ATPStorages <tcgytsc@gmail.com>
2024-09-06 16:56:20 -0500

    Fix CPP type using the wrong location

--- common/src/main/kotlin/bread/mod/breadmod/networking/definition/war_timer/WarTimerSynchronization.kt
+++ common/src/main/kotlin/bread/mod/breadmod/networking/definition/war_timer/WarTimerSynchronization.kt
@@ -1,6 +1,6 @@
 package bread.mod.breadmod.networking.definition.war_timer
 
-import bread.mod.breadmod.ModMainCommon
+import bread.mod.breadmod.ModMainCommon.modLocation
 import dev.architectury.networking.NetworkManager.NetworkReceiver
 import io.netty.buffer.ByteBuf
 import net.minecraft.network.codec.ByteBufCodecs
@@ -10,7 +10,7 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload
 internal data class WarTimerSynchronization(val time: Int?) : CustomPacketPayload {
     companion object {
         val TYPE: CustomPacketPayload.Type<WarTimerSynchronization> =
-            CustomPacketPayload.createType(ModMainCommon.MOD_ID + "_war_timer_sync")
+            CustomPacketPayload.Type(modLocation("war_timer_sync"))
 
         val STREAM_CODEC: StreamCodec<ByteBuf, WarTimerSynchronization> = StreamCodec.composite(
             ByteBufCodecs.VAR_INT,
