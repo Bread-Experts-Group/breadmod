8d4b7a0a30ad07b786661d3bfe256167d4460366
ATPStorages <tcgytsc@gmail.com>
2024-09-07 23:13:25 -0500

    :skull:

--- fabric/src/main/kotlin/bread/mod/breadmod/fabric/datagen/SmartSoundProviderFabric.kt
+++ fabric/src/main/kotlin/bread/mod/breadmod/fabric/datagen/SmartSoundProviderFabric.kt
@@ -1,27 +1,64 @@
 package bread.mod.breadmod.fabric.datagen
 
-import bread.mod.breadmod.datagen.sound.SoundProvider
-import bread.mod.breadmod.fabric.FabricSoundProvider
+import bread.mod.breadmod.datagen.sound.DataGenerateSound
+import bread.mod.breadmod.datagen.sound.SmartSoundProvider
+import com.google.common.hash.HashCode
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.minecraft.data.CachedOutput
+import net.minecraft.data.DataProvider
+import net.minecraft.sounds.SoundEvent
+import java.util.concurrent.CompletableFuture
 
 class SmartSoundProviderFabric(
     modID: String, forClassLoader: ClassLoader, forPackage: Package
-) : SoundProvider<FabricDataGenerator.Pack>(modID, forClassLoader, forPackage) {
-    override fun generate(forEvent: FabricDataGenerator.Pack) {
-        getSoundMap().forEach { (sound, data) ->
-            forEvent.addProvider { dataOutput, future ->
-                object : FabricSoundProvider(dataOutput, future) {
-                    override fun registerSounds() {
-                        add(
-                            data.name,
-                            sound,
-                            data.volume,
-                            data.pitch,
-                            data.stream
-                        )
-                    }
+) : SmartSoundProvider<FabricDataGenerator.Pack>(modID, forClassLoader, forPackage) {
+    inner class FabricSoundProvider(
+        private val dataOutput: FabricDataOutput,
+        private val soundStore: Map<SoundEvent, Array<DataGenerateSound>>
+    ) : DataProvider {
+        override fun run(output: CachedOutput): CompletableFuture<*> = CompletableFuture.runAsync {
+            val obj = JsonObject()
+            soundStore.forEach { (event, data) ->
+                val subObj = JsonObject()
+                subObj.addProperty("subtitle", event.location.toLanguageKey("sound"))
+
+                val sounds = JsonArray()
+                data.forEach {
+                    val soundObj = JsonObject()
+                    soundObj.addProperty("name", modID + ":" + it.sound)
+                    if (it.type != DataGenerateSound.Type.FILE) soundObj.addProperty("type", it.type.name.lowercase())
+
+                    if (it.volume != 1.0f) soundObj.addProperty("volume", it.volume)
+                    if (it.pitch != 1.0f) soundObj.addProperty("pitch", it.pitch)
+                    if (it.weight != 1) soundObj.addProperty("weight", it.weight)
+                    if (it.stream) soundObj.addProperty("stream", true)
+                    if (it.attenuation_distance != 16) soundObj.addProperty("attenuation_distance", it.attenuation_distance)
+                    if (it.preload) soundObj.addProperty("preload", true)
+
+                    if (soundObj.size() == 1) sounds.add(soundObj.getAsJsonPrimitive("name").asString)
+                    else sounds.add(soundObj)
                 }
+                subObj.add("sounds", sounds)
+
+                obj.add(event.location.path, subObj)
             }
+
+            output.writeIfNeeded(
+                dataOutput.outputFolder.resolve("sounds.json"),
+                GsonBuilder().setPrettyPrinting().create().toJson(obj).encodeToByteArray(),
+                HashCode.fromInt(Gson().toJson(obj).encodeToByteArray().contentHashCode())
+            )
         }
+
+        override fun getName(): String = "Sound Provider"
+    }
+
+    override fun generate(forEvent: FabricDataGenerator.Pack) {
+        forEvent.addProvider { dataOutput, _ -> FabricSoundProvider(dataOutput, getSoundMap()) }
     }
 }
--- neoforge/src/main/kotlin/bread/mod/breadmod/neoforge/datagen/SmartSoundProviderNeoForge.kt
+++ neoforge/src/main/kotlin/bread/mod/breadmod/neoforge/datagen/SmartSoundProviderNeoForge.kt
@@ -1,30 +1,37 @@
 package bread.mod.breadmod.neoforge.datagen
 
-import bread.mod.breadmod.ModMainCommon
-import bread.mod.breadmod.datagen.sound.SoundProvider
+import bread.mod.breadmod.datagen.sound.DataGenerateSound
+import bread.mod.breadmod.datagen.sound.SmartSoundProvider
+import net.neoforged.neoforge.common.data.SoundDefinition
+import net.neoforged.neoforge.common.data.SoundDefinition.SoundType
 import net.neoforged.neoforge.common.data.SoundDefinitionsProvider
 import net.neoforged.neoforge.data.event.GatherDataEvent
 
 class SmartSoundProviderNeoForge(
     modID: String, forClassLoader: ClassLoader, forPackage: Package
-) : SoundProvider<GatherDataEvent>(modID, forClassLoader, forPackage) {
+) : SmartSoundProvider<GatherDataEvent>(modID, forClassLoader, forPackage) {
     override fun generate(forEvent: GatherDataEvent) {
-        getSoundMap().forEach { (sound, data) ->
-            val soundProvider = object : SoundDefinitionsProvider(
-                forEvent.generator.packOutput, modID, forEvent.existingFileHelper) {
-                override fun registerSounds() {
-                    add(sound, definition()
-                        .subtitle("sound.${ModMainCommon.MOD_ID}.${data.name}")
-                        .with(sound(sound.location)
-                            .volume(data.volume)
-                            .pitch(data.pitch)
-                            .stream(data.stream)
-                        )
+        forEvent.generator.addProvider(true, object : SoundDefinitionsProvider(
+            forEvent.generator.packOutput, modID, forEvent.existingFileHelper) {
+            override fun registerSounds() = getSoundMap().forEach { (event, annotations) ->
+                val definition = SoundDefinition.definition()
+                    .subtitle(event.location.toLanguageKey("sound"))
+                annotations.forEach {
+                    definition.with(
+                        sound(
+                            modID + ":" + it.sound,
+                            if (it.type == DataGenerateSound.Type.FILE) SoundType.SOUND else SoundType.EVENT
+                        ).volume(it.volume)
+                            .pitch(it.pitch)
+                            .weight(it.weight)
+                            .stream(it.stream)
+                            .attenuationDistance(it.attenuation_distance)
+                            .preload(it.preload)
                     )
                 }
-            }
 
-            forEvent.generator.addProvider(true, soundProvider)
-        }
+                add(event, definition)
+            }
+        })
     }
 }
