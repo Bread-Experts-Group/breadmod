627f9a5d489f8e65ba7ef0950b296f64c35c0506
ATPStorages <tcgytsc@gmail.com>
2025-02-11 03:56:46 -0600

    Add 32-bit support to 0x89 MOV instruction

--- src/main/kotlin/org/bread_experts_group/breadmod/experimental/computer/ia32/instruction/H89InstructionMOV.kt
+++ src/main/kotlin/org/bread_experts_group/breadmod/experimental/computer/ia32/instruction/H89InstructionMOV.kt
@@ -4,12 +4,20 @@ import org.bread_experts_group.breadmod.experimental.computer.ia32.IA32Processor
 
 object H89InstructionMOV {
 	fun handle(processor: IA32Processor) {
-		if (processor.csOverride || processor.bitOverride) TODO("can't support CS/66")
+		if (processor.csOverride) TODO("can't support CS")
 		processor.fetch()
-		val rm = processor.decoding.getModRM16A(processor.cir, DecodingUtil.AddressingLength.R16)
-		rm.memRM.decide(
-			{ it.set(rm.register.get()) },
-			{ processor.computer.setMemoryAt16(it, rm.register.get().toUShort()) }
-		)
+		if (processor.bitOverride) {
+			val rm = processor.decoding.getModRM16A(processor.cir, DecodingUtil.AddressingLength.R32)
+			rm.memRM.decide(
+				{ it.set(rm.register.get()) },
+				{ processor.computer.setMemoryAt32(it, rm.register.get().toUInt()) }
+			)
+		} else {
+			val rm = processor.decoding.getModRM16A(processor.cir, DecodingUtil.AddressingLength.R16)
+			rm.memRM.decide(
+				{ it.set(rm.register.get()) },
+				{ processor.computer.setMemoryAt16(it, rm.register.get().toUShort()) }
+			)
+		}
 	}
 }
