54446c47c4d35828e9a83b746bfcad0871cd882b
ATPStorages <tcgytsc@gmail.com>
2025-04-20 19:14:23 -0500

    deadly changes
    Signed-off-by: ATPStorages <tcgytsc@gmail.com>

--- src/main/java/org/bread_experts_group/breadmod/mixin/common/MixinEntity.java
+++ src/main/java/org/bread_experts_group/breadmod/mixin/common/MixinEntity.java
@@ -1,21 +1,16 @@
 package org.bread_experts_group.breadmod.mixin.common;
 
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.Vec3;
+import com.google.common.collect.AbstractIterator;
+import net.minecraft.world.level.BlockCollisions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Entity.class)
-abstract class MixinEntity {
-	@Inject(method = "isInWall", at = @At("HEAD"), cancellable = true)
-	private void isInWall(CallbackInfoReturnable<? super Boolean> cir) {
-		// TODO apply fake block logic to this
-	}
-
-	@Inject(method = "collide", at = @At("RETURN"), cancellable = true)
-	private void collide(Vec3 vec, CallbackInfoReturnable<Vec3> cir) {
-		// TODO apply fake block logic to this
+@Mixin(BlockCollisions.class)
+abstract class MixinBlockCollisions<T> extends AbstractIterator<T> {
+	@Inject(method = "computeNext", at = @At("HEAD"), cancellable = true)
+	private void computeNext(CallbackInfoReturnable<? super T> cir) {
+		cir.setReturnValue(this.endOfData());
 	}
 }
