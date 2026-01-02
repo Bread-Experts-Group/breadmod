35192a179355bc14e947514b715d00db453c815d
ttttdoy <logan3mclean@gmail.com>
2026-01-01 22:47:12 -0500

    redirect Container#stillValidBlockEntity

---
+++ src/main/java/org/bread_experts_group/breadmod/mixin/common/MixinContainer.java
@@ -0,0 +1,21 @@
+package org.bread_experts_group.breadmod.mixin.common;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import org.bread_experts_group.breadmod.experimental.physics_grid.PhysicsGrid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Container.class)
+interface MixinContainer {
+	@ModifyReturnValue(
+			method = "stillValidBlockEntity(Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/player/Player;F)Z",
+			at = @At("RETURN")
+	)
+	private static boolean redirectStillValid(boolean original, @Local(argsOnly = true) Player player) {
+		PhysicsGrid grid = PhysicsGrid.getClosestGrid(player);
+		return grid != null || original;
+	}
+}
