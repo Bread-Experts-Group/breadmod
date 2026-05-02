e4fdb898e22a8f5aab79cc9e26f2883877189167
ATPStorages <tcgytsc@gmail.com>
2026-05-02 11:29:28 -0500

    upd 6
    Signed-off-by: ATPStorages <tcgytsc@gmail.com>

--- upwards/src/main/java/org/bread_experts_group/upwards/UpwardsLanguageLoader.java
+++ upwards/src/main/java/org/bread_experts_group/upwards/UpwardsLanguageLoader.java
@@ -1,33 +1,18 @@
 package org.bread_experts_group.upwards;
 
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.BusBuilder;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.ModLoadingException;
 import net.neoforged.fml.ModLoadingIssue;
 import net.neoforged.fml.event.IModBusEvent;
-import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforgespi.language.IModInfo;
 import net.neoforged.neoforgespi.language.IModLanguageLoader;
 import net.neoforged.neoforgespi.language.ModFileScanData;
 import net.neoforged.neoforgespi.locating.IModFile;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.lang.annotation.ElementType;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Parameter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.jar.JarFile;
-import java.util.stream.Stream;
 
 public final class UpwardsLanguageLoader implements IModLanguageLoader {
 	@Override
@@ -38,20 +23,6 @@ public final class UpwardsLanguageLoader implements IModLanguageLoader {
 	@Override
 	public String version() {
 		return "a";
-	}
-
-	private void addPath(ArrayList<Object> classPath, Path path) {
-		if (Files.isDirectory(path)) {
-			classPath.add(path);
-			return;
-		}
-		try {
-			Path temp = Files.createTempFile(null, "jar");
-			Files.copy(path, temp, StandardCopyOption.REPLACE_EXISTING);
-			classPath.add(new JarFile(temp.toFile()));
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		}
 	}
 
 	@Override
@@ -71,85 +42,10 @@ public final class UpwardsLanguageLoader implements IModLanguageLoader {
 						)
 				)
 		);
-		Map<String, Object> annotationData = annotation.annotationData();
-		ModContainer container;
-		try {
-			ArrayList<Object> localClassPath = new ArrayList<>();
-			Path modPath = modFile.getFilePath();
-			if (modPath.endsWith("java/main")) localClassPath.add(modPath.resolve("../../kotlin/main"));
-			addPath(localClassPath, modPath);
-			Path path = modFile.findResource((String) annotationData.get("dependencyLocation"));
-			try (Stream<Path> fileList = Files.list(path)) {
-				fileList.forEach(entry -> addPath(localClassPath, entry));
-			}
-			Object[] paths = localClassPath.toArray(new Object[0]);
-
-			String piggyback = (String) annotationData.get("piggybackModID");
-			ClassLoader parent;
-			if (piggyback != null) {
-				parent = ((UpwardsModContainer) ModList.get().getModContainerById(piggyback).orElseThrow()).classLoader;
-			} else {
-				parent = this.getClass().getClassLoader();
-			}
-			ClassLoader classLoader = new LimitedClassLoader(paths, parent);
-
-			Class<?> modClass = classLoader.loadClass(annotation.memberName());
-			Constructor<?>[] constructors = modClass.getConstructors();
-			if (constructors.length != 1) throw new ModLoadingException(
-					new ModLoadingIssue(
-							ModLoadingIssue.Severity.ERROR,
-							"something.here.todo.miko2",
-							Collections.emptyList(),
-							null, null, modFile, info
-					)
-			);
-			Constructor<?> constructor = constructors[0];
-			HashSet<Class<?>> availableClasses = new HashSet<>(3);
-			availableClasses.add(IEventBus.class);
-			availableClasses.add(ModContainer.class);
-			availableClasses.add(Dist.class);
-			Object[] parameters = new Object[constructor.getParameterCount()];
-			Parameter[] constructorParameters = constructor.getParameters();
-
-			IEventBus eventBus = BusBuilder.builder()
-					.markerType(IModBusEvent.class)
-					.allowPerPhasePost()
-					.build();
-			container = new UpwardsModContainer(info, eventBus, parameters, constructor);
-
-			int i = 0;
-			while (i < parameters.length) {
-				Parameter parameter = constructorParameters[i];
-				if (!availableClasses.remove(parameter.getType())) throw new ModLoadingException(
-						new ModLoadingIssue(
-								ModLoadingIssue.Severity.ERROR,
-								"something.here.todo.miko3",
-								Collections.emptyList(),
-								null, null, modFile, info
-						)
-				);
-				else {
-					Class<?> type = parameter.getType();
-					if (type == ModContainer.class) {
-						parameters[i] = container;
-					} else if (type == IEventBus.class) {
-						parameters[i] = eventBus;
-					} else if (type == Dist.class) {
-						parameters[i] = FMLEnvironment.dist;
-					}
-				}
-				i++;
-			}
-			return container;
-		} catch (Exception e) {
-			throw new ModLoadingException(
-					new ModLoadingIssue(
-							ModLoadingIssue.Severity.ERROR,
-							"something.here.todo.miko4",
-							Collections.emptyList(),
-							e, null, modFile, info
-					)
-			);
-		}
+		IEventBus eventBus = BusBuilder.builder()
+				.markerType(IModBusEvent.class)
+				.allowPerPhasePost()
+				.build();
+		return new UpwardsModContainer(info, eventBus, annotation);
 	}
 }
--- upwards/src/main/java/org/bread_experts_group/upwards/UpwardsModContainer.java
+++ upwards/src/main/java/org/bread_experts_group/upwards/UpwardsModContainer.java
@@ -1,27 +1,46 @@
 package org.bread_experts_group.upwards;
 
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.ModLoadingException;
+import net.neoforged.fml.ModLoadingIssue;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforgespi.language.IModInfo;
+import net.neoforged.neoforgespi.language.ModFileScanData;
+import net.neoforged.neoforgespi.locating.IModFile;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.jar.JarFile;
+import java.util.stream.Stream;
 
 final class UpwardsModContainer extends ModContainer {
 	private final IEventBus modBus;
-	private final Object[] constructorParameters;
-	private final Constructor<?> modConstructor;
+	private final ModFileScanData.AnnotationData annotation;
 
 	@SuppressWarnings("InstanceVariableMayNotBeInitialized")
-	ClassLoader classLoader;
+	private ClassLoader classLoader;
 
 	UpwardsModContainer(
-			IModInfo info, IEventBus modBus, Object[] constructorParameters, Constructor<?> modConstructor
+			IModInfo info,
+			IEventBus modBus,
+			ModFileScanData.AnnotationData annotation
 	) {
 		super(info);
 		this.modBus = modBus;
-		this.constructorParameters = constructorParameters;
-		this.modConstructor = modConstructor;
+		this.annotation = annotation;
 	}
 
 	@Override
@@ -29,13 +48,97 @@ final class UpwardsModContainer extends ModContainer {
 		return this.modBus;
 	}
 
+	private void addPath(ArrayList<Object> classPath, Path path) {
+		if (Files.isDirectory(path)) {
+			classPath.add(path);
+			return;
+		}
+		try {
+			Path temp = Files.createTempFile(null, "jar");
+			Files.copy(path, temp, StandardCopyOption.REPLACE_EXISTING);
+			classPath.add(new JarFile(temp.toFile()));
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
 	@Override
 	protected void constructMod() {
+		IModFile modFile = this.modInfo.getOwningFile().getFile();
+		Map<String, Object> annotationData = this.annotation.annotationData();
 		try {
-			Object mod = this.modConstructor.newInstance(this.constructorParameters);
+			ArrayList<Object> localClassPath = new ArrayList<>();
+			Path modPath = modFile.getFilePath();
+			if (modPath.endsWith("java/main")) localClassPath.add(modPath.resolve("../../kotlin/main"));
+			addPath(localClassPath, modPath);
+			Path path = modFile.findResource((String) annotationData.get("dependencyLocation"));
+			try (Stream<Path> fileList = Files.list(path)) {
+				fileList.forEach(entry -> addPath(localClassPath, entry));
+			}
+			Object[] paths = localClassPath.toArray(new Object[0]);
+
+			String piggyback = (String) annotationData.get("piggybackModID");
+			ClassLoader parent;
+			if (piggyback != null) {
+				parent = ((UpwardsModContainer) ModList.get().getModContainerById(piggyback).orElseThrow()).classLoader;
+			} else {
+				parent = this.getClass().getClassLoader();
+			}
+			ClassLoader classLoader = new LimitedClassLoader(paths, parent);
+
+			Class<?> modClass = classLoader.loadClass(this.annotation.memberName());
+			Constructor<?>[] constructors = modClass.getConstructors();
+			if (constructors.length != 1) throw new ModLoadingException(
+					new ModLoadingIssue(
+							ModLoadingIssue.Severity.ERROR,
+							"something.here.todo.miko2",
+							Collections.emptyList(),
+							null, null, modFile, this.modInfo
+					)
+			);
+			Constructor<?> constructor = constructors[0];
+			HashSet<Class<?>> availableClasses = new HashSet<>(3);
+			availableClasses.add(IEventBus.class);
+			availableClasses.add(ModContainer.class);
+			availableClasses.add(Dist.class);
+			Object[] parameters = new Object[constructor.getParameterCount()];
+			Parameter[] constructorParameters = constructor.getParameters();
+
+			int i = 0;
+			while (i < parameters.length) {
+				Parameter parameter = constructorParameters[i];
+				if (!availableClasses.remove(parameter.getType())) throw new ModLoadingException(
+						new ModLoadingIssue(
+								ModLoadingIssue.Severity.ERROR,
+								"something.here.todo.miko3",
+								Collections.emptyList(),
+								null, null, modFile, this.modInfo
+						)
+				);
+				else {
+					Class<?> type = parameter.getType();
+					if (type == ModContainer.class) {
+						parameters[i] = this;
+					} else if (type == IEventBus.class) {
+						parameters[i] = this.modBus;
+					} else if (type == Dist.class) {
+						parameters[i] = FMLEnvironment.dist;
+					}
+				}
+				i++;
+			}
+
+			Object mod = constructor.newInstance(parameters);
 			this.classLoader = mod.getClass().getClassLoader();
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new ModLoadingException(
+					new ModLoadingIssue(
+							ModLoadingIssue.Severity.ERROR,
+							"something.here.todo.miko4",
+							Collections.emptyList(),
+							e, null, modFile, this.modInfo
+					)
+			);
 		}
 	}
 }
